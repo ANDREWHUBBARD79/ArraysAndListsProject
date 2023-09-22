916ef2b5c7d2efc45da077ff11fde030ca3189e0
Andrew <andrewhubbard78@gmail.com>
2023-09-22 16:14:40 -0400

    A

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace ArraysAndLists
 {
@@ -27,6 +29,55 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+            
+            var numbers = new int[10] {1,2,3, 4, 5, 6 ,7,8,9,10};
+
+            var evens = new List<int>();
+            var odds = new List<int>();
+
+            foreach (var num in numbers)
+            {
+             if(num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+             else
+                {
+                    odds.Add(num);
+                }
+
+             //
+             //for(int i = 0; i < numbers.Length; i++)
+             //   {
+             //       if (numbers[i] % 2 == 0)
+             //       {
+             //        evens.Add(numbers)[i];
+             //       }
+
+             //       else
+             //       {
+             //           odds.Add(numbers)[i];
+             //       }
+             //   }
+            
+            }
+
+            foreach (var item in evens)
+            {
+
+                Console.WriteLine($"{item} is an even number");
+            
+            }
+
+            foreach (var oddNumber in odds)
+            {
+             Console.WriteLine($"{oddNumber} is def odd");
+
+            }
+
+
+
+
         }
     }
 }
